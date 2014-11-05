68cc9418f3f0f125044e00aa20da76d152144023
Simon Bothen <caresilabs@gmail.com>
2014-11-05 20:05:18 +0100

    Some Documentation

---
+++ MAH_Platformer/MAH_Platformer/Entities/Entity.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MAH_Platformer.Entities
+{
+    class Entity
+    {
+       /* public bool PixelCollition(Entity other)
+        {
+            Color[] dataA = new Color[tex.Width * tex.Height];
+            tex.GetData(dataA);
+            Color[] dataB = new Color[other.tex.Width * other.tex.Height];
+            other.tex.GetData(dataB);
+            int top = Math.Max(hitBox.Top, other.hitBox.Top);
+            int bottom = Math.Min(hitBox.Bottom, other.hitBox.Bottom);
+            int left = Math.Max(hitBox.Left, other.hitBox.Left);
+            int right = Math.Min(hitBox.Right, other.hitBox.Right);
+            for (int y = top; y < bottom; y++)
+            {
+                for (int x = left; x < right; x++)
+                {
+                    Color colorA = dataA[(x - hitBox.Left) +
+                    (y - hitBox.Top) * hitBox.Width];
+                    Color colorB = dataB[(x - other.hitBox.Left) +
+                   (y - other.hitBox.Top) *
+                   other.hitBox.Width];
+                    if (colorA.A != 0 && colorB.A != 0)
+                    {
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }*/
+    }
+}
